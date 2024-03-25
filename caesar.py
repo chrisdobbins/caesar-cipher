c10037d9705f500bb91c06a2f779cabb5df2864f
CJD <christopherjosephdobbins@gmail.com>
2024-03-25 09:35:57 -0400

    add overengineered solution

---
+++ caesar.py
@@ -0,0 +1,24 @@
+def cipher(input: str):
+  offset = -3
+  start_lower = ord('a') # 97
+  end_lower = ord('z')   # 122
+  start_upper = ord('A') # 65
+  end_upper = ord('Z') # 90
+  output = ""
+  for ch in input:
+    start = 0
+    end = 0
+    if ord(ch) >= start_lower and ord(ch) <= end_lower:
+      start = start_lower
+      end = end_lower
+    elif ord(ch) >= start_upper and ord(ch) <= end_upper:
+      start = start_upper
+      end = end_upper
+    else:
+      output = output + ch
+      continue
+    transform = start + ((ord(ch) - start + offset)%(end-start+1))
+    output = output + chr(transform)
+  print(output)
+
+cipher("abcd Wxyz")
